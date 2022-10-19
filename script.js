9c26a7e793756e01eba3382b9a1682bf37d619b2
Olga <example@gmail.com>
2022-10-19 18:58:46 +0300

    first practice in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log(1);
+
+
+const numberFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMuvieDB = {
+    count:numberFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false,
+
+};
+
+const answer1 = prompt('Один из последних просмотренных фильмов?','');
+const answer2 = prompt('На сколько оцените  его','');
+const answer3 = prompt('Один из последних просмотренных фильмов?','');
+const answer4 = prompt('на сколько оцените его','');
+
+personalMuvieDB[answer1]= answer2;
+personalMuvieDB[answer3]=answer4;
+
+console.log(personalMuvieDB);
